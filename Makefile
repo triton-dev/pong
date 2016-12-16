fab65416ba31473e432abd0cd7ab79ed04da184d
gyuri <szikoragy@gmail.com>
2016-12-16 21:42:34 +0100

    bump mod

--- Makefile
+++ Makefile
@@ -3,11 +3,17 @@ CFLAGS = -std=c++11
 OUT = -o pong
 LIBS = -lncurses
 
+bump: bump.o
+	$(CC) $(CFLAGS) $(LIBS) bump.o -o bump
+
 pong: pong.o 
 	$(CC) $(CFLAGS) $(LIBS) *.o $(OUT)
 
 pong.o: pong.c    
 	$(CC) $(CFLAGS) -c pong.c
+	
+bump.o: bump.c
+	$(CC) $(CFLAGS) $(LIBS) -c bump.c
 
 
 .PHONY: clean
--- bump.c
+++ bump.c
@@ -7,14 +7,14 @@ class Ball {
   char shape;
   
   void move(int w, int h) {
-    mvprintw(y,x,"%c",' ');
+    //mvprintw(y,x,"%c",' ');
     x += dx;
     y += dy;
     mvprintw(y,x,"%c",shape);
-    if(x >= w - 1 || x <= 1 ) {
+    if(x >= w - 1 || x < 1 ) {
       dx = -dx;
     }
-    if(y >= h - 1 || y <= 1) {
+    if(y >= h - 1 || y < 1) {
       dy = -dy;
     }
   }
@@ -36,12 +36,17 @@ class Ball {
 int main() {
   
   int mag, szel;
-  Ball b[5] = {
-    Ball(15,17,1,-1,255),
-    Ball(15,12,-1,-1,'#'),
-    Ball(12,10,1,1,'o'),
-    Ball(18,16,1,-1,'*'),
-    Ball(13,19,-1,-1,'+')
+  Ball b[10] = {
+    Ball(15,17,1,-1,'O'),
+    Ball(15,12,-1,-1,'O'),
+    Ball(12,10,1,1,'O'),
+    Ball(18,16,-1,1,'O'),
+    Ball(13,19,1,1,'O'),
+    Ball(13,19,-1,-1,'O'),
+    Ball(3,7,1,-1,'O'),
+    Ball(5,7,-1,1,'O'),
+    Ball(11,7,-1,-1,'O'),
+    Ball(1,5,1,1,'O')
   };
   
   initscr();
@@ -49,15 +54,17 @@ int main() {
   raw();
   keypad(stdscr, TRUE);
   noecho();
+  nodelay(stdscr, TRUE);
   char key;
+  int bsize = 10;
+  printw("%d",bsize);
   
   while((key = getch()) != 27) {
-      for(int i=0; i<5; i++) {
+      for(int i=0; i<bsize; i++) {
       getmaxyx(stdscr,mag,szel);
-      //mvprintw(0,0,"%d, y=%d, dx=%d, dy=%d, shape=%c", b.x, b.y, b.dx, b.dy, b.shape);
       b[i].move(szel, mag);
       refresh();
-      usleep(5000);
+      usleep(2500);
     }
   }
   getch();
